87138074d55484faba6bfe38b2c4f897d33a8f3d
katevel <kate.vel.m@gmail.com>
2011-11-05 19:59:16 -0300

    se agrego el menu

--- system/index.php
+++ system/index.php
@@ -4,49 +4,10 @@
 <title>Random TEST</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="../css/style.css" />
-<link rel="stylesheet" type="text/css" href="../css/menu.css" />
-<link rel="stylesheet" type="text/css" href="../js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="../js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript" src="../js/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-<script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
-<script type="text/javascript"> 
-            $(function() {
-                $('#navigation > div').hover(
-                function () {
-                    var $this = $(this);
-					//$this.find('.images').fadeIn();
-					
-					$this.find('a.menu').removeClass('menu').addClass('hovered');
-					
-                    $this.find('.images').stop().animate({
-                        'width'     :'230px',
-                        'height'    :'390px',
-                        'opacity'   :'1.0'
-                    },400,'easeOutBack',function(){
-					
-                        $(this).parent().find('div').fadeIn('fast');
-                    });
-                },
-                function () {
-                    var $this = $(this);
-					
-                    $this.find('div').fadeOut(500);
-					//$this.find('.images').hide();
-                   $this.find('a.hovered').removeClass('hovered').addClass('menu');
-				   
-				    $this.find('.images').stop().animate({
-                        'width'     :'100px',
-                        'height'    :'0px',
-                        'top'       :'0px',
-                        'left'      :'0px',
-                        'opacity'   :'0.9'
-                    },600,'easeOutBack'); 
-              }
-            );
-            });
-</script>
-
+    <link type="text/css" href="menu.css" rel="stylesheet" />
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="menu.js"></script>
+</style>
 <!-------------------------------------------->
 </head>
 <body>
@@ -55,120 +16,61 @@
 	</div>
 	<div id="main">
 		<div id="menu">
-			<div class="total_images" id="navigation">
-			 
-                <div class="eachs" id="link1"> 
-					
-					<a href="#" class="menu" style=" width:90px;">99Points</a>
-					
-                    <img src="../img/bg.png" alt="" width="140" height="140" class="images"/> 
-					
-                    <div> 
-                        <ul>
-							<li><a href="#">PHP</a></li>
-							<li><a href="#">CodeIgniter</a></li>
-							<li><a href="#">JQuery</a></li>
-							<li><a href="#">AJAX</a></li>
-							<li><a href="#">Facebook</a></li>
-							
-							<li><a href="#">YOUTUBE</a></li>
-							<li><a href="#">CSS</a></li>
-							<li><a href="#">MYSQL</a></li>
-							<li><a href="#">Tutorials</a></li>
-						</ul>
-                    </div>  
-                </div> 
-				
-				<div class="eachs" id="link4"> 
-				
-					<a href="#" class="menu" style=" width:90px;">JQuery</a>
-					
-                    <img src="../img/bg.png" alt="" width="140" height="140" class="images"/> 
-                   
-                    <div>
-                        <ul>
-							<li><a href="#">PHP</a></li>
-							<li><a href="#">CodeIgniter</a></li>
-							<li><a href="#">JQuery</a></li>
-							<li><a href="#">AJAX</a></li>
-							<li><a href="#">Facebook</a></li>
-							
-							<li><a href="#">YOUTUBE</a></li>
-							<li><a href="#">CSS</a></li>
-							<li><a href="#">MYSQL</a></li>
-							<li><a href="#">Tutorials</a></li>
-						</ul>                    </div> 
-                </div> 
-				
-				
-                <div class="eachs" id="link2"> 
-					
-					<a href="#" class="menu" style=" width:100px;">FaceBook</a>
-					
-                    <img src="../img/bg.png" alt="" width="160" height="199" class="images"/> 
-                    
-                    <div> 
-                        <ul>
-							<li><a href="#">PHP</a></li>
-							<li><a href="#">CodeIgniter</a></li>
-							<li><a href="#">JQuery</a></li>
-							<li><a href="#">AJAX</a></li>
-							<li><a href="#">Facebook</a></li>
-							
-							<li><a href="#">YOUTUBE</a></li>
-							<li><a href="#">CSS</a></li>
-							<li><a href="#">MYSQL</a></li>
-							<li><a href="#">Tutorials</a></li>
-						</ul>
-						                    </div>  
-                </div> 
-				
-				
-                <div class="eachs" id="link3"> 
-					
-					<a href="#" class="menu" style=" width:72px;">PHP</a>
-					
-                    <img src="../img/bg.png" alt="" width="160" height="199" class="images"/> 
-                   
-                    <div> 
-                        <ul>
-							<li><a href="#">PHP</a></li>
-							<li><a href="#">CodeIgniter</a></li>
-							<li><a href="#">JQuery</a></li>
-							<li><a href="#">AJAX</a></li>
-							<li><a href="#">Facebook</a></li>
-							
-							<li><a href="#">YOUTUBE</a></li>
-							<li><a href="#">CSS</a></li>
-							<li><a href="#">MYSQL</a></li>
-							<li><a href="#">Tutorials</a></li>
-						</ul>                    </div> 
-                </div> 
-               
-                <div class="eachs" id="link5"> 
-					
-					<a href="#" class="menu" style=" width:84px;">AJAX</a>
-					
-                    <img src="../img/bg.png" alt="" width="160" height="199" class="images"/> 
-                    
-                    <div> 
-                        <ul>
-							<li><a href="#">PHP</a></li>
-							<li><a href="#">CodeIgniter</a></li>
-							<li><a href="#">JQuery</a></li>
-							<li><a href="#">AJAX</a></li>
-							<li><a href="#">Facebook</a></li>
-							
-							<li><a href="#">YOUTUBE</a></li>
-							<li><a href="#">CSS</a></li>
-							<li><a href="#">MYSQL</a></li>
-							<li><a href="#">Tutorials</a></li>
-						</ul>                    
-					</div> 
-                </div> 
-				<br clear="all" /><br clear="all" /><br clear="all" /><br clear="all" />
-            </div>
-		</div>
+				 <ul class="menu">
+        <li><a href="#" class="parent"><span>Home</span></a>
+            <div><ul>
+                <li><a href="#" class="parent"><span>Sub Item 1</span></a>
+                    <div><ul>
+                        <li><a href="#" class="parent"><span>Sub Item 1.1</span></a>
+                            <div><ul>
+                                <li><a href="#"><span>Sub Item 1.1.1</span></a></li>
+                                <li><a href="#"><span>Sub Item 1.1.2</span></a></li>
+                            </ul></div>
+                        </li>
+                        <li><a href="#"><span>Sub Item 1.2</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.3</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.4</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.5</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.6</span></a></li>
+                        <li><a href="#" class="parent"><span>Sub Item 1.7</span></a>
+                            <div><ul>
+                                <li><a href="#"><span>Sub Item 1.7.1</span></a></li>
+                                <li><a href="#"><span>Sub Item 1.7.2</span></a></li>
+                            </ul></div>
+                        </li>
+                    </ul></div>
+                </li>
+                <li><a href="#"><span>Sub Item 2</span></a></li>
+                <li><a href="#"><span>Sub Item 3</span></a></li>
+            </ul></div>
+        </li>
+        <li><a href="#" class="parent"><span>Product Info</span></a>
+            <div><ul>
+                <li><a href="#" class="parent"><span>Sub Item 1</span></a>
+                    <div><ul>
+                        <li><a href="#"><span>Sub Item 1.1</span></a></li>
+                        <li><a href="#"><span>Sub Item 1.2</span></a></li>
+                    </ul></div>
+                </li>
+                <li><a href="#" class="parent"><span>Sub Item 2</span></a>
+                    <div><ul>
+                        <li><a href="#"><span>Sub Item 2.1</span></a></li>
+                        <li><a href="#"><span>Sub Item 2.2</span></a></li>
+                    </ul></div>
+                </li>
+                <li><a href="#"><span>Sub Item 3</span></a></li>
+                <li><a href="#"><span>Sub Item 4</span></a></li>
+                <li><a href="#"><span>Sub Item 5</span></a></li>
+                <li><a href="#"><span>Sub Item 6</span></a></li>
+                <li><a href="#"><span>Sub Item 7</span></a></li>
+            </ul></div>
+        </li>
+        <li><a href="#"><span>Help</span></a></li>
+        <li class="last"><a href="#"><span>Contacts</span></a></li>
+    </ul>
+		
+		
+		</div>	
 		
 		<div id="content-system">
 			
