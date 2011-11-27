2cfeae21f4686875b26812b26542aa47fbaabee3
Pablo Salgado <pablito.salgado@gmail.com>
2011-11-27 17:34:20 -0400

    fun search

--- system/admin/func.search.php
+++ system/admin/func.search.php
@@ -1,10 +1,25 @@
 <?php
 
-$term = $_GET['tern'];
+$term = $_GET['term'];
 
-
-$query = mysql_query("");
-
-
+$query = mysql_query("SELECT 
+					  Alumno.idalumno,
+					  Alumno.rut_alumno,
+					  Alumno.nombre_alumno,
+					  Alumno.apePat_alumno,
+					  Alumno.apePat_alumno 
+					FROM
+					  alumno Alumno 
+					WHERE Alumno.rut_alumno = '".$term."' 
+					ORDER BY Alumno.nombre_alumno ");
+					
+	$row = mysql_fetch_assoc($query);
+	
+	echo "<tr>
+			<td>".$row['Alumno']['nombre_alumno']."</td>
+			<td>".$row['Alumno']['apePat_alumno']."</td>
+			<td>".$row['Alumno']['rut_alumno']."</td>
+			<td><a href='Javascript: agregar('".$row['Alumno']['idalumno']."');'>Agregar</a></td>
+		 </td>";
 
 ?>
