9fdc1d1b5346c75a1057db28e98cfd178887bd01
pabloncio <pablito.salgado@gmail.com>
2011-11-28 23:26:36 -0300

    form validate

--- system/admin/agregar_alumno.php
+++ system/admin/agregar_alumno.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" type="text/css" href="../css/formvalidate.css" />
-<script type="text/javascript" src="../js/jquery.js"></script>
 <script type="text/javascript" src="../js/jquery.validate.js"></script>
 <script type="text/javascript" src="../js/jquery.Rut.js"></script>
 
