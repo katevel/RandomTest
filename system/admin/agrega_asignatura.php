906c8b4b1b2d17d44b57e9a6758306199e2da1bd
katevel <kate.vel.m@gmail.com>
2011-12-03 22:46:39 -0300

    ingreso profe_asig

--- system/admin/agrega_asignatura.php
+++ system/admin/agrega_asignatura.php
@@ -13,11 +13,24 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 if(!empty($nombre)){
 	
 	$query = mysql_query("INSERT INTO asignatura ( idasignatura	,
-													nombre_asignatura,
-												 descripcion_asignatura, 
-												 profesor_idprofesor
+												   nombre_asignatura,
+												   descripcion_asignatura
 												 ) 
-						 VALUES('', '".utf8_encode($nombre)."' , '".utf8_encode($descripcion)."', '$idprofe' )")or die(mysql_error());
+						 VALUES('', '".utf8_encode($nombre)."' , '".utf8_encode($descripcion)."' )")or die(mysql_error());
+	
+	$query2 = mysql_query("SELECT idasignatura FROM asignatura WHERE nombre_asignatura = '".utf8_encode($nombre)."' order by idasignatura desc limit 1")or die(mysql_error());
+	
+		while($row = mysql_fetch_assoc($query2)){
+		@$idasignatura= $row['idasignatura'];
+		$query3 = mysql_query("INSERT INTO profesor_has_asignatura ( id_profe_asig,
+																	profesor_idprofesor,
+												   					asignatura_idasignatura
+												 					) 
+						 VALUES('','$idprofe' , '$idasignatura' )")or die(mysql_error());
+
+	}
+	
+	
 	
 }else{
 	echo "error";
