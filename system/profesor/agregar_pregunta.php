e25ae0d85df588dd85aa684b7c1c50ea13c2ac6c
pabloncio <pablito.salgado@gmail.com>
2011-12-06 00:39:32 -0300

    estilos y formulario preguntas

--- system/profesor/agregar_pregunta.php
+++ system/profesor/agregar_pregunta.php
@@ -13,6 +13,7 @@ $qdificultad = mysql_query("SELECT
 
 ?>
 <form class="form-style" id="form" method="post" action="agrega_asignatura.php">
+<input type="hidden" name="id-docente" value="<?=$_SESSION['user_id']?>" />
 <h2>Agregar Pregunta</h2>
 <ul>
     <li class="first">
@@ -39,7 +40,34 @@ $qdificultad = mysql_query("SELECT
     	</select> <br />
         </p>
     </li>
-    
+    <li>
+    	<h3>Alternativa 1</h3>
+    	<p> 
+    		<textarea id="alternativa1" name="alternativa1" rows="2" cols="2"></textarea>
+    		<label class="emb">Correcta</label><input type="checkbox" name="check1" class="emb-chck" data-id="1"/>
+    	</p>
+    </li>
+    <li>
+    	<h3>Alternativa 2</h3>
+    	<p>
+    		<textarea id="alternativa2" name="alternativa2" rows="2" cols="2"></textarea>
+    		<label class="emb">Correcta</label><input type="checkbox" name="check2" class="emb-chck" data-id="2"/>
+    	</p>
+    </li>
+    <li>
+    	<h3>Alternativa 3</h3>
+    	<p>
+    		<textarea id="alternativa3" name="alternativa3" rows="2" cols="2"></textarea>
+    		<label class="emb">Correcta</label><input type="checkbox" name="check3" class="emb-chck" data-id="3"/>
+    	</p>
+    </li>
+    <li>
+    	<h3>Alternativa 4</h3>
+    	<p>
+    		<textarea id="alternativa4" name="alternativa4" rows="2" cols="2"></textarea>
+    		<label class="emb">Correcta</label><input type="checkbox" name="check4" class="emb-chck" data-id="4"/>
+    	</p>
+    </li>
     <li class="last">
         <input id="guardar" value="Guardar" class="submit" type="submit">
     </li>
