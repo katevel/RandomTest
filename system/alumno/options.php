3e35ca005465d8a2b8bff6c1d6dbaf21b33d0634
pabloncio <pablito.salgado@gmail.com>
2011-12-12 22:59:13 -0300

    resolver

---
+++ system/alumno/options.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require("../conexion.php");
+$cn = conectar();
+
+function getAlternatives($question_id){
+	$query = mysql_query("SELECT
+						  Alternativa.idalternativas,
+						  Alternativa.alternativa
+						FROM alternativas Alternativa
+						WHERE Alternativa.pregunta_idPregunta = ".$question_id."
+						ORDER BY RAND()")or die(mysql_error());
+	$result = array();
+
+	while($row = mysql_fetch_array($query)){
+		$result['id'][] = $row[0];
+		$result['alternativa'][] = $row[1];
+	}
+	return $result; 
+}
+
--- system/alumno/resolver.php
+++ system/alumno/resolver.php
@@ -1,10 +1,10 @@
 <?php 
 session_start();
-require("../conexion.php");
-$cn = conectar(); 
+include("options.php");
+ 
  
 $query = mysql_query("SELECT
-						  Prueba.idprueba,
+						  Prueba.idprueba
 						FROM prueba Prueba
 						WHERE Prueba.alumno_idalumno = ".$_SESSION['user_id']."
 						    AND Prueba.estado = 'ACTIVA'")or die(mysql_error());
@@ -27,3 +27,24 @@ while($row_detalle = mysql_fetch_array($query_detalle)){
 	$contenidos[] 	= $row_detalle[1];
 	$cantidades[] 	= $row_detalle[2];
 }
+$join_level = join(",",$dificultades);
+$join_content = join(",",$contenidos);
+$sum_cant = array_sum($cantidades);
+
+$query_preguntas = mysql_query("SELECT Pregunta.idPregunta, Pregunta.pregunta
+								FROM pregunta Pregunta
+								WHERE Pregunta.dificultad_iddificultad IN(".$join_level.")
+								    AND Pregunta.contenido_idcontenido IN(".$join_content.")
+								ORDER BY RAND()
+								LIMIT ".$sum_cant."
+								")or die(mysql_error());
+	
+	while($row_pregunta = mysql_fetch_array($query_preguntas)){
+			$preguntas['id'][] = $row_pregunta[0];
+			$preguntas['pregunta'][] = $row_pregunta[1];
+			$preguntas['alternativas'][] = getAlternatives($row_pregunta[0]);
+	}								
+								
+	echo "<pre>"; print_r($preguntas); echo "</pre>";
+								
+								
